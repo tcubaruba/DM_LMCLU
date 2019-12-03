dc82d05da731d9a2cac8d05cab5831bea2b18d86
a1204151 <a1204151@unet.univie.ac.at>
2019-12-03 16:50:38 +0100

    initial commit

--- lmclu/lmclu.py
+++ lmclu/lmclu.py
@@ -1,2 +1,7 @@
+from find_separation.find_separation import find_separation
 def lmclu(dataset, max_lm_dim, sampling_level, sensitivity_threshold):
+
+    #blabla
+    #for loop:
+    find_separation()
     return
