f3345b910b948accb38829df89b06c0a6048cd55
a1204151 <a1204151@unet.univie.ac.at>
2019-12-03 20:27:27 +0100

    orthonormal besis changed

--- find_separation/form_orthonormal_basis.py
+++ find_separation/form_orthonormal_basis.py
@@ -6,8 +6,6 @@ def form_orthonormal_basis(M, O):
     for row in M:
         if((np.array_equal(row, O))):
             space.append(row)
-    # space = np.delete(M, O, axis=0)
-    # print(space)
     return np.linalg.qr(space)
 
 
