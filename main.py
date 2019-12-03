c00fd74cca2fbdcf0397daf2d78f9d65788f20ff
Thomas Fenz <thomas.fenz@univie.ac.at>
2019-12-03 20:59:26 +0100

    implemented main

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import pandas as pd
 from lmclu import lmclu
 
+__K = 2
+__S = 1
+__Gamma = 0
+
+# __file_name = "data/vary-density.csv"
 __file_name = "data/mouse.csv"
-__file_name = "data/vary-density.csv"
 
-def load_data(filename):
-    return pd.read_csv()
 
-lmclu.lmclu()
+def load_data(file_path):
+    df = pd.read_csv(file_path, names=['a1', 'a2', 'label'], header=None)
+    return df
+
+
+def find_cluster(df, K, S, Gamma):
+    clusters, lm_dims = lmclu.lmclu(df, K, S, Gamma)
+    return clusters, lm_dims
+
 
 if __name__ == '__main__':
+    df = load_data(__file_name)
+    clusters, lm_dims = find_cluster(df, __K, __S, __Gamma)
+    print("clusters: ")
+    print(clusters)
+
+    print("lm_dims: ")
+    print(lm_dims)
     pass
