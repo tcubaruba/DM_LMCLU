a2e83429bd0d9364ad970f0c7edf7ee2bbed93b0
Thomas Fenz <thomas.fenz@univie.ac.at>
2019-12-08 15:57:56 +0100

    beautified main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from lmclu import lmclu
 
-__K = 2
-__S = 2
-__Gamma = 0
+__K = 2  # max LM dim
+__S = 2  # sampling level
+__Gamma = 0  # sensitivity threshold
 
 # __file_name = "data/vary-density.csv"
 __file_name = "data/mouse.csv"
@@ -14,17 +14,21 @@ def load_data(file_path):
     return df
 
 
-def find_cluster(df, K, S, Gamma):
+def invoke_lmclus(df, K, S, Gamma):
     clusters, lm_dims = lmclu.lmclu(df, K, S, Gamma)
     return clusters, lm_dims
 
 
-if __name__ == '__main__':
+def main():
     df = load_data(__file_name)
-    clusters, lm_dims = find_cluster(df, __K, __S, __Gamma)
+    clusters, lm_dims = invoke_lmclus(df, __K, __S, __Gamma)
+
     print("clusters: ")
     print(clusters)
 
     print("lm_dims: ")
     print(lm_dims)
-    pass
+
+
+if __name__ == '__main__':
+    main()
