f95f9f31e8457beefa167c1888edee61fefe6bf4
Thomas Fenz <thomas.fenz@univie.ac.at>
2019-12-08 15:08:04 +0100

    added vague strategy of evaluate goodness

--- find_separation/evaluate_goodness_of_separation.py
+++ find_separation/evaluate_goodness_of_separation.py
@@ -1,4 +1,8 @@
 def evaluate_goodness_of_separation(T, H):
+    # step 1: calculate discriminability [see paper (6)]
+    # step 2: depth = J(tau') - J(tau)
+    # step 3: G = discriminablility x depth [see paper (7)]
+
     G = 0
     return G
 
